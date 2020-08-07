4054b0168f1febf74d88d85d24ec74cd8167d3b7
David Shen <davidshen84@gmail.com>
2020-08-07 21:00:25 +1000

    Fix DNS firewall rules

--- iptables-init.sh
+++ iptables-init.sh
@@ -16,7 +16,8 @@ iptables -A INPUT -p icmp --icmp-type 12 -j ACCEPT -m comment --comment "Paramet
 iptables -A INPUT -p udp -m multiport --sports 67,68 -m multiport --dports 67,68 -j ACCEPT -m comment --comment 'DHCP'
 iptables -A INPUT -p udp --sport 1900 -j ACCEPT -m comment --comment 'UPnP source'
 iptables -A INPUT -p udp --dport 1900 -j ACCEPT -m comment --comment 'UPnP destination'
-iptables -R INPUT -p udp -s 192.168.86.0/24 --sport 9999 --dport 9999 -j ACCEPT -m comment --comment 'Google Home'
+iptables -A INPUT -p udp -s 192.168.86.0/24 --sport 9999 --dport 9999 -j ACCEPT -m comment --comment 'Google Home'
+iptables -A INPUT -p udp -s 192.168.86.0/24 --sport 5353 --dport 5353 -j ACCEPT
 iptables -A INPUT -p udp -m multiport --dports 137,138 -j ACCEPT -m comment --comment 'samba service sports'
 iptables -A INPUT -p tcp -m multiport --dports 139,445 -j ACCEPT -m comment --comment 'samba service sports'
 iptables -A INPUT -p tcp --syn --dport 113 -j REJECT --reject-with tcp-reset -m comment --comment "Reject IDENTITY request probing"
