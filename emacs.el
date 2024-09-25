62d5558aed030e045b9ec20c064f25137f2700dd
David Shen <davidshen84@gmail.com>
2024-09-25 00:40:57 +0000

    update: treemacs config

--- emacs.el
+++ emacs.el
@@ -371,7 +371,6 @@
     :ensure t)
 
   :bind (:map global-map
-              ("M-0"       . treemacs-select-window)
               ("C-x t 1"   . treemacs-delete-other-windows)
               ("C-x t t"   . treemacs)
               ("C-x t B"   . treemacs-bookmark)
