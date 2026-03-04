26cf73f6e7c5a425d27e9b9c1cf9a8eaa70ae79c
David Shen <davidshen84@gmail.com>
2026-03-04 11:03:23 +1100

    chore: remove fram maximize setting

--- home/.config/emacs/init.el
+++ home/.config/emacs/init.el
@@ -28,7 +28,6 @@
                 (menu-bar-mode -1)
                 (tool-bar-mode -1)
                 (scroll-bar-mode -1)
-                (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 
                 (set-face-attribute
                  'default nil
@@ -464,18 +463,6 @@
 
 ;;; .emacs ends here
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars noruntime)
